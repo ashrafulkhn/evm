efd09f8efdccd767b04ec8a3c3f51e9f41be7421
ashrafulkhn <ashrafulkhn@gmail.com>
2024-12-03 03:33:56 +0530

    Embedded programs are started.

--- main_ashraf_0312.py
+++ main_ashraf_0312.py
@@ -7,6 +7,200 @@ import os, time, random
 # from print_actions import print_image
 from escpos.printer import *
 
+import RPi.GPIO as GPIO
+
+# ========EXTERNAL I/O DEVICE FUNCTIONS======
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set GPIO for different Purposes
+top_motor_pin       = 12
+bottom_motor_pin    = 13
+top_glass_pin       = 27
+bottom_glass_pin    = 17
+green_led_pin       = 6
+red_led_pin         = 5
+reset_button_pin    = 16
+
+
+# Set GPIO mode for respective GPIOs
+GPIO.setup  (top_motor_pin,       GPIO.OUT)
+GPIO.setup  (bottom_motor_pin,    GPIO.OUT)
+GPIO.setup  (top_glass_pin,       GPIO.OUT)
+GPIO.setup  (bottom_glass_pin,    GPIO.OUT)
+GPIO.setup  (green_led_pin,       GPIO.OUT)
+GPIO.setup  (red_led_pin,         GPIO.OUT)
+GPIO.setup  (reset_button_pin,    GPIO.IN)
+
+# Set Initial State for the Output GPIO.
+GPIO.output(top_glass_pin, GPIO.HIGH)
+GPIO.output(bottom_glass_pin, GPIO.HIGH)
+GPIO.output(green_led_pin, GPIO.LOW)
+GPIO.output(red_led_pin, GPIO.HIGH)
+
+# =====FUNCTIONS=======
+def reset_pi():
+    reset_val = GPIO.input(reset_button_pin)
+    if reset_val == 1:
+        system("sudo reboot")
+
+def top_motor():
+    pwm_top = GPIO.PWM(top_motor_pin, 50)
+    pwm_top.start(6)
+    time_delay_top = decrement_value_in_file(file_path_top)
+    print(time_delay_top)
+    time.sleep(time_delay_top)
+    pwm_top.stop()
+
+def top_motor_re():
+    pwm_top = GPIO.PWM(top_motor_pin, 52)
+    pwm_top.start(6)
+    time_delay_top = decrement_value_in_file(file_path_top)
+    print(time_delay_top)
+    time.sleep(3)
+    pwm_top.stop()
+
+def bottom_motor():
+    pwm_bottom = GPIO.PWM(bottom_motor_pin, 50)
+    pwm_bottom.start(8)
+    time_delay_bottom = decrement_value_in_file(file_path_bottom)
+    print(time_delay_bottom)
+    time.sleep(time_delay_bottom)
+    pwm_bottom.stop()
+    
+def bottom_motor_re():
+    pwm_bottom = GPIO.PWM(bottom_motor_pin, 52)
+    pwm_bottom.start(8)
+    time_delay_bottom = decrement_value_in_file(file_path_bottom)
+    print(time_delay_bottom)
+    time.sleep(3)
+    pwm_bottom.stop()
+
+def top_glass_on():
+    GPIO.output(top_glass_pin, GPIO.LOW)
+   # time.sleep(6)
+   # GPIO.output(glass_T, GPIO.HIGH)
+    
+def top_glass_off():
+    GPIO.output(top_glass_pin, GPIO.HIGH)
+
+def bottom_glass_on():
+    GPIO.output(bottom_glass_pin, GPIO.LOW)
+   # time.sleep(6)
+    #GPIO.output(glass_B, GPIO.HIGH)
+    
+def bottom_glass_off():
+    GPIO.output(bottom_glass_pin, GPIO.HIGH)
+
+# =========PRINTER===========================
+# PRINTER SETUP
+# Printer 1 Path in Linux
+p1 = File("/dev/usb/lp0")
+p1.set(font='a',align="center",width=1,height=1)
+
+# Printer 2 Path in Linux
+p2 = File("/dev/usb/lp1")
+p2.set(font='a',align="center",width=1,height=1)
+
+def print_image(img_path):
+    global print_select	
+    rand = random.randint(1, 2)
+    GPIO.output(led_r, GPIO.LOW)
+    GPIO.output(led_g, GPIO.HIGH)
+    if rand == 1:
+        print_select = 1	
+    elif rand == 2:
+        print_select = 2
+    if print_select == 1:
+     #   p1.writelines(vote_status + "\n")
+        p1._raw(b'\x1b@')
+        p1.set(align='center')
+        p1.set(font='a', align = "center", width=1, height=1)
+        p1._raw(b'\x1b\x21x')
+        p1.image(img_path)
+        bottom_motor_re()
+     #   time.sleep(1)
+        time.sleep(3)
+        #bottom_motor()
+        bottom_glass_on()
+        
+        message = confirm()
+        p1._raw(b'\n')
+        ean = barcode.get('ean13', '123456789012', writer=ImageWriter())
+        filename = ean.save('barcode')
+        barcode_image = Image.open(filename)
+        barcode_image = barcode_image.rotate(90, expand=True)  
+        new_size = (100,200)
+        barcode_image = barcode_image.resize(new_size, Image.ANTIALIAS)
+       
+        barcode_image.save('resized_rotated_barcode.png')
+        combined_width = barcode_image.width + 200  
+        combined_height = max(barcode_image.height, 50)  
+        combined_image = Image.new('RGB', (combined_width, combined_height), 'white')
+        barcode_x_position = -30
+        combined_image.paste(barcode_image, (barcode_x_position, 0))
+        draw = ImageDraw.Draw(combined_image)
+        font_size = 58
+        font = ImageFont.truetype("usr/share/fonts/truetype/msttcorefonts/Arial.ttf", 24)  # Load a default font; adjust as necessary
+        text_position = (barcode_image.width + 10, (combined_height - font_size) // 2)  # Adjust text position
+        draw.text(text_position, message, fill='black', font=font)
+        combined_image.save('../combined_image.png')
+        p1.image('../combined_image.png')
+        barcode_datåa = "12345678"
+        p1._raw(b'\x1dV\x00')
+        p1.cut()
+        p1.flush()
+        #time.sleep(0.1)
+        bottom_motor()
+        time.sleep(6)
+        bottom_glass_off()
+        GPIO.output(led_r, GPIO.HIGH)
+        GPIO.output(led_g, GPIO.LOW)
+
+    elif print_select == 2:
+        p2._raw(b'\x1b@')
+        p2.set(align='center')
+        p2.set(font='a', align = "center", width=1, height=1)
+        p2._raw(b'\x1b\x21x')
+        p2.image(img_path)
+        time.sleep(1)
+        top_motor_re()
+        time.sleep(3)
+        top_glass_on()
+        #top_motor
+        message = confirm()
+        p2._raw(b'\n')
+        ean = barcode.get('ean13', '123456789012', writer=ImageWriter())
+        filename = ean.save('barcode')
+        barcode_image = Image.open(filename)
+        barcode_image = barcode_image.rotate(90, expand=True)  
+        new_size = (100,200)
+        barcode_image = barcode_image.resize(new_size, Image.ANTIALIAS)
+       
+        barcode_image.save('../resized_rotated_barcode.png')
+        combined_width = barcode_image.width + 200  
+        combined_height = max(barcode_image.height, 50)  
+        combined_image = Image.new('RGB', (combined_width, combined_height), 'white')
+        barcode_x_position = -30
+        combined_image.paste(barcode_image, (barcode_x_position, 0))
+        draw = ImageDraw.Draw(combined_image)
+        font_size = 58
+        font = ImageFont.truetype("usr/share/fonts/truetype/msttcorefonts/Arial.ttf", 24)  # Load a default font; adjust as necessary
+        text_position = (barcode_image.width + 10, (combined_height - font_size) // 2)  # Adjust text position
+        draw.text(text_position, message, fill='black', font=font)
+        combined_image.save('../combined_image.png')
+        p2.image('../combined_image.png')
+        barcode_datåa = "12345678"
+        p2._raw(b'\x1dV\x00')
+        p2.cut()
+        p2.flush()
+        top_motor()
+        time.sleep(6)
+        top_glass_off()
+        GPIO.output(led_r, GPIO.HIGH)
+        GPIO.output(led_g, GPIO.LOW)
+
+
 # ============GUI Related Fuctions==========
 # ========ROOT WINDOW CREATION==============
 root = tk.ThemedTk()
@@ -515,7 +709,7 @@ def voting_terminated_screen(base_frame):
 
     # Start Timer
     start_timer(frame1, 5, time_label, lambda: open_vote_window(base_frame))
-    # print("The voting has been terminated.")
+    print("The voting has been terminated.")
 
 # Continue the main script
 base_frame = Frame(root, bg="white")
